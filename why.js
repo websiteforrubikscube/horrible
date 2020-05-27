8d17434726bfb838df97115141a5a537f91a4975
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-05-27 08:21:43 -0400

    Update why.js

--- why.js
+++ why.js
@@ -1,5 +1,7 @@
 export default function launch(){
 constructor(){
+ this.yoshi=0;
+ this.rickroll=0;
  keys = {};
     keys[70] = false;
       window.onkeyup = function (e) {
@@ -14,11 +16,16 @@ this.fpress=false;
 let canv = document.getElementById("Screen");
   this.ctx = canv.getContext('2d');
   if((keys[70]==false)&&(this.fpress==false)){
+   if(this.yoshi<1){
+    new Audio('./Super Mario World 2 Yoshi's Island Music Main Theme.mp3').play()
+   }
+   
 this.ctx.fillStyle=("#000");
  this.ctx.fillText("Press F to...",50%,50%);
  }
  if(this.fpress==true){
 this.ctx.fillStyle=("#000");
  this.ctx.fillText("get RICKROLLED!",50%,50%);
+  new Audio('./Rick Astley - Never Gonna Give You Up (Video).mp3').play()
  }
 }
